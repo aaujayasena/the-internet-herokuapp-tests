de5fb7d7655db1ee1e370c8d5884cba4246a8686
unknown <aaujayasena@gmail.com>
2025-11-27 12:24:19 +0530

    Add test

--- cypress/e2e/1-Coding-Assessment/1.1-landing-page-validation.cy.js
+++ cypress/e2e/1-Coding-Assessment/1.1-landing-page-validation.cy.js
@@ -4,7 +4,8 @@ import { LandingPage } from  '../../page-object/landing-page';
 import  {ABTestingPage} from '../../page-object/abtesting-page';
 //Importing Page Content Constants
 import { PageContent } from '../../constant/page-content';
-
+//Importing Add and Remove Page Object Model
+import { AddAndRemovePage} from '../../page-object/add-and-remove-page';
 
 describe('Coding Assigmnet', () => {
     
@@ -12,6 +13,7 @@ describe('Coding Assigmnet', () => {
      const landingPage = new LandingPage();
      //Creating object of ABTestingPage class
      const abtestingPage = new ABTestingPage();
+
 
     beforeEach(() => {
         //Navigate to Landing Page before each test
@@ -31,6 +33,25 @@ describe('Coding Assigmnet', () => {
         landingPage.getlinkabtesting().should('be.visible').click();   
         //Validating A/B Testing Page Header
         abtestingPage.getHeader1().should('be.visible').and('contain.text', PageContent.AB_TESTING_PAGE_HEADER);
+    })
+
+    it('1.1.3 - Ensure user can perfome add and remove actions', () => {
+       
+        //Clicking on Add and Remove Elements link
+        landingPage.getlinkaddandremove().should('be.visible').click({force:true}); 
+        const addAndRemovePage = new AddAndRemovePage();
+        //Validating Add and Remove Page Header
+        addAndRemovePage.getheader1().should('be.visible').and('have.text',PageContent.ADD_AND_REMOVE_PAGE_HEADER); 
+        // Delete button should not be visible initially
+        addAndRemovePage.getDeleteButton().should('not.exist');
+        //Clicking on 'Add Element' button
+        addAndRemovePage.getAddElementButton().should('be.visible').click();
+        //Validating 'Delete' button is displayed
+        addAndRemovePage.getDeleteButton().should('be.visible').and('have.text', PageContent.DELETE);
+        //Clicking on 'Delete' button
+        addAndRemovePage.getDeleteButton().click();
+        //Validating 'Delete' button is removed
+        addAndRemovePage.getDeleteButton().should('not.exist');
     })
 
     //Clearing cookies after each test
